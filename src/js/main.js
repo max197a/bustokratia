6e89c679fb2ee21dda843c909630bc621fa39bd4
max197a <max197aa@gmail.com>
2019-03-06 01:07:57 +0200

    FULL CARD PAGE

--- src/js/main.js
+++ src/js/main.js
@@ -30,7 +30,7 @@ $(document).ready(function() {
     initSlider();
     initPopup();
     initQuantity();
-    // initZoom();
+    initZoom();
   }
 
   // this is a master function which should have all functionality
@@ -67,6 +67,18 @@ $(document).ready(function() {
       scrollToSection($(el));
       return false;
     })
+
+    ///
+    /// toggle class на странице карточки товара справа кнопка "Полное описание"
+
+    .on("click", "[js-thumb-toggle]", function(e) {
+      e.preventDefault();
+      $("[js-thumb-toggle]").removeClass("is-active");
+      $(this).toggleClass("is-active");
+    })
+
+    /// toggle class на странице карточки товара справа кнопка "Полное описание"
+    ///
 
     ///
     /// toggle class на странице карточки товара справа кнопка "Полное описание"
@@ -336,107 +348,54 @@ $(document).ready(function() {
   });
 
   function initZoom() {
-    $(".xzoom").xzoom({
-      // top, left, right, bottom, inside, lens, fullscreen, #ID
-      position: "right",
+    $(document).ready(function() {
+      $(".xzoom, .xzoom-gallery").xzoom({
+        zoomWidth: 500,
+        zoomHeight: 500,
+        title: true,
+        tint: "#333",
+        Xoffset: 15
+      });
 
-      // inside, fullscreen
-      mposition: "inside",
+      //Integration with hammer.js
+      var isTouchSupported = "ontouchstart" in window;
 
-      // In the HTML structure, this option gives an ability to output xzoom element, to the end of the document body or relative to the parent element of main source image.
-      rootOutput: true,
+      if (isTouchSupported) {
+        //If touch device
+        $(".xzoom").each(function() {
+          var xzoom = $(this).data("xzoom");
+          xzoom.eventunbind();
+        });
 
-      // x/y offset
-      Xoffset: 0,
-      Yoffset: 0,
+        $(".xzoom").each(function() {
+          var xzoom = $(this).data("xzoom");
+          $(this)
+            .hammer()
+            .on("tap", function(event) {
+              event.pageX = event.gesture.center.pageX;
+              event.pageY = event.gesture.center.pageY;
+              var s = 1,
+                ls;
 
-      // Fade in effect, when zoom is opening.
-      fadeIn: true,
+              xzoom.eventmove = function(element) {
+                element.hammer().on("drag", function(event) {
+                  event.pageX = event.gesture.center.pageX;
+                  event.pageY = event.gesture.center.pageY;
+                  xzoom.movezoom(event);
+                  event.gesture.preventDefault();
+                });
+              };
 
-      // Fade transition effect, when switching images by clicking on thumbnails.
-      fadeTrans: true,
-
-      // Fade out effect, when zoom is closing.
-      fadeOut: false,
-
-      // Enable smooth effects
-      smooth: true,
-
-      // Smooth move effect of the big zoomed image in the zoom output window.
-      // The higher value will make movement smoother.
-      smoothZoomMove: 3,
-
-      // Smooth move effect of lens.
-      smoothLensMove: 1,
-
-      // Smooth move effect of scale.
-      smoothScale: 6,
-
-      // From -1 to 1, that means -100% till 100% scale
-      defaultScale: 0,
-
-      // Scale on mouse scroll.
-      scroll: true,
-
-      // Tint color. Color must be provided in format like "#color".
-      tint: false,
-
-      // Tint opacity from 0 to 1.
-      tintOpacity: 0.5,
-
-      // Lens color. Color must be provided in format like "#color".
-      lens: false,
-
-      // Lens opacity from 0 to 1.
-      lensOpacity: 0.5,
-
-      // 'box', 'circle'
-      lensShape: "box",
-
-      // Custom width of zoom window in pixels.
-      zoomWidth: "auto",
-
-      // Custom height of zoom window in pixels.
-      zoomHeight: "auto",
-
-      // Class name for source "div" container.
-      sourceClass: "xzoom-source",
-
-      // Class name for loading "div" container that appear before zoom opens, when image is still loading.
-      loadingClass: "xzoom-loading",
-
-      // Class name for lens "div".
-      lensClass: "xzoom-lens",
-
-      // Class name for zoom window(div).
-      zoomClass: "xzoom-preview",
-
-      // Class name that will be added to active thumbnail image.
-      activeClass: "xactive",
-
-      // With this option you can make a selection action on thumbnail by hover mouse point on it.
-      hover: false,
-
-      // Adaptive functionality.
-      adaptive: true,
-
-      // When selected position "inside" and this option is set to true, the lens direction of moving will be reversed.
-      lensReverse: false,
-
-      // Same as lensReverse, but only available when adaptive is true.
-      adaptiveReverse: false,
-
-      // Lens will collide and not go out of main image borders. This option is always false for position "lens".
-      lensCollision: true,
-
-      //  Output title/caption of the image, in the zoom output window.
-      title: false,
-
-      // Class name for caption "div" container.
-      titleClass: "xzoom-caption",
-
-      // Zoom image output as background
-      bg: false
+              xzoom.eventleave = function(element) {
+                element.hammer().on("tap", function(event) {
+                  xzoom.closezoom();
+                });
+              };
+              xzoom.openzoom(event);
+            });
+        });
+      } else {
+      }
     });
   }
 
